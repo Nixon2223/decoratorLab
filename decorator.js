485c12c61450987f9bb3271acd0524f900810370
Nixon2223 <n.nixon@hotmail.co.uk>
2022-01-05 15:56:14 +0000

    calculate total paint added

--- decorator.js
+++ decorator.js
@@ -6,4 +6,12 @@ Decorator.prototype.addPaint = function(paint){
     this.paints.push(paint)
 }
 
+Decorator.prototype.totalPaint = function(){
+    let total = 0
+    for (paint of this.paints){
+        total += paint.litres
+    }
+    return total
+}
+
 module.exports = Decorator
--- specs/decorator_specs.js
+++ specs/decorator_specs.js
@@ -18,4 +18,23 @@ describe('decorator', function(){
         const actual = decorator.paints
         assert.deepStrictEqual(actual, [paint])
     })
+
+    describe('Paint', function(){
+
+        beforeEach (function(){
+            paint2 = new Paint(10)
+            paint3 = new Paint(5)
+            paint4 = new Paint(5)
+            decorator.addPaint(paint2)
+            decorator.addPaint(paint3)
+            decorator.addPaint(paint4)
+        })
+    
+    it('should return total litres of paint', function(){
+        const actual = decorator.totalPaint()
+        assert.strictEqual(actual, 20)
+    })
+
+
+    })
 })
